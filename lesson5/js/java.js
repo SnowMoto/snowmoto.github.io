743620b81c43a6213510f31b73ffc5295f6273b7
Chelsea Thomson <63980230+SnowMoto@users.noreply.github.com>
2020-05-24 09:47:45 -0600

    Fixed Date format

--- lesson5/js/java.js
+++ lesson5/js/java.js
@@ -1,4 +1,13 @@
 function toggleMenu(){
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
     }
-    
+{
+let today = new Date();
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// document.getElementById("demo").innerHTML = days[d.getDay()];
+
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// document.getElementById("month").innerHTML = months[d.getMonth()];
+
+let date = days[today.getDay()] + ', ' + today.getDate() + " " + months[today.getMonth()] + " " + today.getFullYear();
+document.getElementById("date").innerHTML = date;}
