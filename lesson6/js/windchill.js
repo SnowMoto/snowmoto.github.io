689e921c611a93577d6aaa2bba598903c7271a87
SnowMoto <motogirlmonday@gmail.com>
2020-05-28 20:19:19 -0600

    added if and else

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,4 +1,4 @@
-function windChill(){
+/*function windChill(){
     let temp=45;
     let mph=10;
     let result=chillWind(temp,mph);
@@ -7,4 +7,15 @@ function windChill(){
 function chillWind(temp,mph){
     let result= 35.74+(0.6215*temp)-35.75*Math.pow(mph,0.16)+0.4275*temp*Math.pow(mph,0.16);
     return result;
-}
+}*/
+function windChill(){
+let temp= parseFloat(document.getElementById("temp").textContent);
+let mph= parseFloat(document.getElementById("wspd").textContent);
+
+    if(temp <= 50 && mph >= 3){
+    let f=35.74+(0.6215*temp)-35.75*Math.pow(mph,0.16)+0.4275*temp*Math.pow(mph,0.16);
+    let result = f.toFixed(0);
+    document.getElementById('chilly').innerHTML = result + '&#8457;';
+} else {
+    document.getElementById('chilly').textContent = "N/A";
+}}
