e5468bd2e79ad96bba6a309e88758d2cd9ffc1c2
SnowMoto <motogirlmonday@gmail.com>
2020-06-04 21:04:34 -0600

    java fix

--- lesson7/javascript/lazyjavascript.js
+++ lesson7/javascript/lazyjavascript.js
@@ -1,8 +1,8 @@
 let imagesToLoad = document.querySelectorAll('img[data-src]');
 
 const imgOptions = {
-    threshold: 1,
-    rootMargin: "0px 0px 40px 0px"
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
 };
 
 const loadImages = (image) => {
