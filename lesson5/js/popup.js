eeb59091939b442dbd7ca7ba785eeb9238c9e66c
SnowMoto <motogirlmonday@gmail.com>
2020-06-20 18:15:30 -0600

    updated tab links

---
+++ lesson5/js/popup.js
@@ -0,0 +1,10 @@
+function addPopup() {
+   let greeting="Hero's Pancake Social at Preston Park, Saturday 8am"
+   let result= "Come back Friday for Pancake Info!";
+   let d=new Date();
+   if (d.getDay()==5){
+      result=alert(greeting);
+   }
+   else{
+      return alert(result);
+   }}
