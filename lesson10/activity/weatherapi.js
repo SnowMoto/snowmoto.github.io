ae23583e925125ac991f11345d76d9cc4520da3e
SnowMoto <motogirlmonday@gmail.com>
2020-06-24 15:48:02 -0600

    api test fix

--- lesson10/activity/weatherapi.js
+++ lesson10/activity/weatherapi.js
@@ -1,13 +1,14 @@
-const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=&APPID=7f3a044384f1f53298096cafcc0b978a';//api url to pull information from
-fetch(apiURL)//to retrieve needed weather info.
-  .then((response) => response.json())
-  .then((jsObject) => {
-    console.log(jsObject);
-   // document.getElementById('current-temp').textContent = jsObject.main.temp;//show current temp in city
+const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=7f3a044384f1f53298096cafcc0b978a';
 
-    //const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-//const desc = jsObject.weather[0].description;  // note how we reference the weather array
-//document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-//document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-//document.getElementById('icon').setAttribute('alt', desc);
-  });
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+        document.getElementById('current-temp').textContent = jsObject.main.temp;
+const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+const desc = jsObject.weather[0].description;  // note how we reference the weather array
+document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+document.getElementById('icon').setAttribute('alt', desc);
+});   
+
