7acee5c717cc1e7e268dafc11319d43ced548e01
SnowMoto <motogirlmonday@gmail.com>
2020-07-17 15:41:21 -0600

    update

--- lesson12/javascript/rental.js
+++ lesson12/javascript/rental.js
@@ -12,15 +12,19 @@ fetch(requestURL)
             let td1 = document.createElement ('td');
             let td2 = document.createElement ('td');
             let td3 = document.createElement ('td');
+            let td4 = document.createElement ('td');
         
-        td1.textContent = rentals [i].name + '' + rentals[i].model +''+ rentals[i].maxseat;
+        td1.textContent = rentals [i].name + ': ' + rentals[i].model;
         tr.appendChild(td1);
         
-        td2.textContent = rentals [i].reserve;
+        td2.textContent = rentals [i].maxseat;
         tr.appendChild(td2);
 
-        td3.textContent = rentals [i].walkin;
+        td3.textContent = rentals [i].reserve;
         tr.appendChild(td3);
+
+        td4.textContent = rentals [i].walkin;
+        tr.appendChild(td4);
 
         document.querySelector('tbody').appendChild(tr);
         
